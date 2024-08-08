f43705c740f44a49b92792528d70a15fb9292398
hariom-ojha23 <hariomojha2301@gmail.com>
2024-08-08 22:22:50 +0530

    Route Pages

---
+++ portfolio/src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            Contact
+        </div>
+    )
+}
+
+export default Contact
---
+++ portfolio/src/app/project/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Project = () => {
+    return (
+        <div>
+            Project
+        </div>
+    )
+}
+
+export default Project
